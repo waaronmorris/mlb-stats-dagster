eeea8340047de0b0b41518659e2971f422fbe6f2
W. Aaron Morris <waaronmorris@gmail.com>
2024-07-04 13:30:23 -0500

    feat(config.schedule-loader):  add configurations for schedule loader

---
+++ mlb_stats/configurations.py
@@ -0,0 +1,17 @@
+from dagster import Config
+
+from pydantic import Field
+import datetime as dt
+
+
+class ScheduleLoaderConfig(Config):
+    start_date: str = Field(
+        description="The start date for the schedule",
+        is_required=True,
+        default= (dt.datetime.now() - dt.timedelta(days=30)).strftime('%Y%m%d')
+    )
+    end_date: str = Field(
+        description="The end date for the schedule",
+        is_required=True,
+        default= dt.datetime.now().strftime('%Y%m%d')
+    )
